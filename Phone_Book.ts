5f60adffbd44820e12c5dd0f295e0d4ffedbc4af
Biko23 <bikojeremy@gmail.com>
2020-10-07 00:38:01 +0400

    Started project working on a phonebook. Created a class phone book that is composed of instances of class contact with methods to view contacts, add contact and edit contac.

---
+++ Phone_Book.ts
@@ -0,0 +1,28 @@
+class Contact {
+    fName : String;
+
+    lName : String;
+
+    phone : String;
+
+    email : String;
+}
+
+class PhoneBook {
+    phoneBook = new Contact
+
+    listContacts(){
+
+    }
+
+    addContact(){
+
+    }
+
+    editContact(){
+
+    }
+
+    
+}
+
