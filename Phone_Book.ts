6fcf4344f2eed93324928cb0c462759469919e4a
Biko23 <bikojeremy@gmail.com>
2020-10-15 16:31:23 +0330

    writing to JSON file other than text file. Also receiving input from user in console using readline.

--- Phone_Book.ts
+++ Phone_Book.ts
@@ -6,62 +6,154 @@ const fs = require ('fs');
 //     input: process.stdin,
 //     output: process.stdout
 //   });
+import * as readLine from "readline";
 
-class Contact {
-    Name : String;
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-    phone : String;
+// rl.question('Name: ', (nameAnswer) => {
+//         let Name = nameAnswer;
+        // console.log(Name);
+    // rl.question('Phone: ', (phoneAnswer) => {
+    //     let Phone = phoneAnswer;
+        // console.log(Phone);
 
-    email : String;
+    
+    // rl.close();
+    // });
 
-    constructor (Name: String, phone: String, email: String){
-        this.Name = Name;
+    // switch(answer.toLowerCase()) {
+    //   case 'y':
+    //     console.log('Super!');
+    //     break;
+    //   case 'n':
+    //     console.log('Sorry! :(');
+    //     break;
+    //   default:
+    //     console.log('Invalid answer!');
+    // }
+    // rl.close();
+//   });
+
+
+class Contact{
+    
+    name: String;
+    phone: String;
+    constructor(name: String, phone: String){
+            
+        this.name = name;
         this.phone = phone;
-        this.email = email;
 
+        // console.log(this);
     }
 }
 
-class PhoneBook {
-    contact: Contact;
+class PhoneBook{
+    
     contacts = [];
 
-    addContact(contact: Contact){
-        // let contact1 = new Contact("Biko Jeremy Biteete", "+256 789940343", "bikojeremy@gmail.com");
-        // rl.question("Phone: ", (phone1) => {
-        //     return phone1;
-        // })
+    addContact(contact : Contact){
+        fs.readFile("phoneBook.json", (err, infor)=>{
+            if(err) throw console.error();
 
-        contact = 
+            this.contacts = JSON.parse(infor);
 
-        this.contacts.push(contact);
+            this.contacts.push(contact);
 
+            console.log(this.contacts);
 
+            infor = JSON.stringify(this.contacts);
+
+            fs.writeFile("phoneBook.json", infor, (err) =>{
+                if(err) throw console.error();
+                })
+            
+        })
+        return this.contacts;
+       // console.log(".......")
     }
-
-    listContacts(){
-        console.log(this.contacts);
-
-    }
-
-    editContact(){
-
-    }
-
-    deleteContact(){
-        
-    }
-
-    
 }
 
-const biko = new PhoneBook();
-biko.addContact({Name: "Biko Jeremy Biteete", phone: "+256 789940343", email: "bikojeremy@gmail.com"});
-biko.listContacts();
-// let contact1 = "This phone book is proving a problem."
-fs.writeFile('phoneBook.txt', Contact, (err) => {
-    if (err) throw console.error();
+rl.question('Name: ', (answer)=>{
+    rl.question('Phone: ', (tel)=>{
+
+        let b = new Contact(answer, tel);
+        let a = new PhoneBook();
+        a.addContact(b);
+        // console.log(a);
+
+        rl.close();
+    })
+}) 
+
+//et c = new Contact("Dex", "54698");
+//let a = new PhoneBook();
+//a.addContact(b);
+// a.addContact(c);
+// class Contact {
+//     Name : String;
+
+//     phone : String;
+
+//     email : String;
+
+//     constructor (Name: String, phone: String, email: String) {
+//         this.Name = Name;
+//         this.phone = phone;
+//         this.email = email;
+
+//     }
+// }
+
+// class PhoneBook {
+//     // contact: Contact;
+//     contacts = [];
+//     contact1 = new Contact("Biko Jeremy Biteete", "+256 789 940343", "bikojeremy@gmail.com")
+
+
+//     addContact() {
+//         // let contact1 = new Contact("Biko Jeremy Biteete", "+256 789940343", "bikojeremy@gmail.com");
+//         // rl.question("Phone: ", (phone1) => {
+//         //     return phone1;
+//         // })
+
+//         //contact = 
+//         this.contacts.push(this.contact1);
+//         console.log(this.contacts);
+//         for(let i = 0; i <= this.contacts.length; i++) {
+//             fs.appendFile('phoneBook.txt', this.contacts[i].Name, (err) => {
+//                 if (err) throw console.error();
+                
+//             });
+//         }
+        
+
+
+//     }
+
+//     listContacts() {
+//         console.log(this.contacts);
+
+//     }
+
+//     editContact() {
+
+//     }
+
+//     deleteContact() {
+        
+//     }
+
     
-});
+// }
+
+// PhoneBook();
+// const biko = new PhoneBook();
+// biko.addContact();
+// biko.listContacts();
+// let contact1 = "This phone book is proving a problem."
 
 
--- phone_Book.js
+++ phone_Book.js
@@ -1,14 +1,41 @@
+"use strict";
+exports.__esModule = true;
 var fs = require('fs');
 // const readLine = require('readline');
 // const rl = readline.createInterface({
 //     input: process.stdin,
 //     output: process.stdout
 //   });
+var readLine = require("readline");
+var rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+// rl.question('Name: ', (nameAnswer) => {
+//         let Name = nameAnswer;
+// console.log(Name);
+// rl.question('Phone: ', (phoneAnswer) => {
+//     let Phone = phoneAnswer;
+// console.log(Phone);
+// rl.close();
+// });
+// switch(answer.toLowerCase()) {
+//   case 'y':
+//     console.log('Super!');
+//     break;
+//   case 'n':
+//     console.log('Sorry! :(');
+//     break;
+//   default:
+//     console.log('Invalid answer!');
+// }
+// rl.close();
+//   });
 var Contact = /** @class */ (function () {
-    function Contact(Name, phone, email) {
-        this.Name = Name;
+    function Contact(name, phone) {
+        this.name = name;
         this.phone = phone;
-        this.email = email;
+        // console.log(this);
     }
     return Contact;
 }());
@@ -17,26 +44,75 @@ var PhoneBook = /** @class */ (function () {
         this.contacts = [];
     }
     PhoneBook.prototype.addContact = function (contact) {
-        // let contact1 = new Contact("Biko Jeremy Biteete", "+256 789940343", "bikojeremy@gmail.com");
-        // rl.question("Phone: ", (phone1) => {
-        //     return phone1;
-        // })
-        this.contacts.push(contact);
-    };
-    PhoneBook.prototype.listContacts = function () {
-        console.log(this.contacts);
-    };
-    PhoneBook.prototype.editContact = function () {
-    };
-    PhoneBook.prototype.deleteContact = function () {
+        var _this = this;
+        fs.readFile("phoneBook.json", function (err, infor) {
+            if (err)
+                throw console.error();
+            _this.contacts = JSON.parse(infor);
+            _this.contacts.push(contact);
+            console.log(_this.contacts);
+            infor = JSON.stringify(_this.contacts);
+            fs.appendFile("phoneBook.json", infor, function (err) {
+                if (err)
+                    throw console.error();
+            });
+        });
+        return this.contacts;
+        // console.log(".......")
     };
     return PhoneBook;
 }());
-var biko = new PhoneBook();
-biko.addContact({ Name: "Biko Jeremy Biteete", phone: "+256 789940343", email: "bikojeremy@gmail.com" });
-biko.listContacts();
-// let contact1 = "This phone book is proving a problem."
-fs.writeFile('phoneBook.txt', Contact, function (err) {
-    if (err)
-        throw console.error();
+rl.question('Name: ', function (answer) {
+    rl.question('Phone: ', function (tel) {
+        var b = new Contact(answer, tel);
+        var a = new PhoneBook();
+        a.addContact(b);
+        // console.log(a);
+        rl.close();
+    });
 });
+//et c = new Contact("Dex", "54698");
+//let a = new PhoneBook();
+//a.addContact(b);
+// a.addContact(c);
+// class Contact {
+//     Name : String;
+//     phone : String;
+//     email : String;
+//     constructor (Name: String, phone: String, email: String) {
+//         this.Name = Name;
+//         this.phone = phone;
+//         this.email = email;
+//     }
+// }
+// class PhoneBook {
+//     // contact: Contact;
+//     contacts = [];
+//     contact1 = new Contact("Biko Jeremy Biteete", "+256 789 940343", "bikojeremy@gmail.com")
+//     addContact() {
+//         // let contact1 = new Contact("Biko Jeremy Biteete", "+256 789940343", "bikojeremy@gmail.com");
+//         // rl.question("Phone: ", (phone1) => {
+//         //     return phone1;
+//         // })
+//         //contact = 
+//         this.contacts.push(this.contact1);
+//         console.log(this.contacts);
+//         for(let i = 0; i <= this.contacts.length; i++) {
+//             fs.appendFile('phoneBook.txt', this.contacts[i].Name, (err) => {
+//                 if (err) throw console.error();
+//             });
+//         }
+//     }
+//     listContacts() {
+//         console.log(this.contacts);
+//     }
+//     editContact() {
+//     }
+//     deleteContact() {
+//     }
+// }
+// PhoneBook();
+// const biko = new PhoneBook();
+// biko.addContact();
+// biko.listContacts();
+// let contact1 = "This phone book is proving a problem."
